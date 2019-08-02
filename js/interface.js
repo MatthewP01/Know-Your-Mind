9a219a6fa00b9655126bab3e3894df7a0f993e95
MatthewP01 <matthew.peoples@yale.edu>
2019-08-01 23:48:33 -0400

    spelling errors in text fixed

--- js/interface.js
+++ js/interface.js
@@ -2,6 +2,7 @@ const currentDisplay = document.querySelector('#current-display')
 const sideBar = document.getElementById('mySidenav')
 const body = document.querySelector('body')
 
+// On click of node, display info about region on side-bar
 currentDisplay.addEventListener('click', function(e){
   if (e.target.className === "fab fa-first-order-alt selector"){
     regionId = e.target.dataset.id
@@ -9,6 +10,7 @@ currentDisplay.addEventListener('click', function(e){
     .then(data => {
       createSidebar(data)
       onClick(e.target)
+      // Change id of target to allow for target styling
       e.target.id = 'selected'
     })
   }
@@ -28,8 +30,7 @@ sideBar.addEventListener('click', function(e){
   }
 })
 
-// FA awesome Icons are not within currentDisplay upon page loaded
-// Modal with app info
+// Just for fun
 body.addEventListener('click', function(e){
   if (e.target.id === "info-circle"){
     document.location.href = 'https://www.youtube.com/watch?v=dQw4w9WgXcQ';
@@ -44,6 +45,7 @@ function fetchRegionData(regionId){
   .then(res => res.json())
 }
 
+// Create the side-bar using the text provided from the fetch
 function createSidebar(promise){
   sideBar.innerHTML =
   `<p class="info-text"><u>Anatomical Feature:</u><br><b class = "anatomical-feature">${promise.name}</b></p>
@@ -58,10 +60,11 @@ function onClick(targetNode){
   body.querySelector('.box').style.opacity = 0.3;
   body.querySelector('.text-effect').style.opacity = 0.3;
   body.querySelector('.icons').style.opacity = 0.3;
-  // currentDisplay.querySelector('.fab fa-first-order-alt selector').style.color = 'yellow';
   currentDisplay.style.opacity = 0.6;
+  body.querySelector('.button-navigation').style.opacity = 0.3;
 }
 
+// When the user closes the sidebar
 function pressXLobe(){
   sideBar.innerHTML = ``
   sideBar.style.width = '0px';
@@ -69,6 +72,7 @@ function pressXLobe(){
   body.querySelector('.text-effect').style.opacity = 0.75;
   body.querySelector('.icons').style.opacity = 1;
   currentDisplay.style.opacity = 1;
+
 }
 
 // Remove opacity when closing sidebar
@@ -80,8 +84,11 @@ function pressX(){
   body.querySelector('.icons').style.opacity = 1;
   currentDisplay.style.opacity = 1;
   document.querySelector('#selected').id = '';
+  body.querySelector('.button-navigation').style.opacity = 1;
 }
 
+
+// Information about the major lobes of the brain
 function createLobeSideBar(){
   sideBar.innerHTML =
   `<p class="lobe-label" id="frontal-lobe"> <u>Frontal Lobe:</u> <br>
@@ -94,21 +101,21 @@ function createLobeSideBar(){
    <p class="lobe-label" id="temporal-lobe"> <u>Temporal Lobe:</u><br>
 
    <i class="lobe-info">
-   The lobe at the base of the brain involved in processing sensory input into derived meanings,
+   The lobe at the base of the brain that is involved in processing sensory input into derived meanings,
    such as in the case of emotion, memory and language comprehension</i></p><br>
 
 
    <p class="lobe-label" id="occipital-lobe"> <u>Occipital Lobe:</u><br>
 
    <i class="lobe-info">
-   In mammals, this lobe is visual processing center of the brain and containing most
+   In mammals, this lobe is the visual processing center of the brain and containing most
    of the anatomical region of the visual cortex, involved in visual decoding</i></p><br>
 
 
    <p class="lobe-label" id="parietal-lobe"> <u>Parietal Lobe:</u><br>
 
    <i class="lobe-info">
-   This is the middle section of the brain and it is associated with processing input from the five senses
+   This is the middle section of the brain and is associated with processing input from the five senses
    as well as proprioception - one's own sense of self</i></p><br>
 
 
